8add455e765f24fbdd8f657abd9d12f8df512564
{Ayush} <{ayushkumargupta2908@gmail.com}>
2024-06-14 22:44:37 +0530

    mongoAtlas connection

---
+++ src/index.js
@@ -0,0 +1,36 @@
+//require('dotenv').config({path: './env'})//runs completely but damages code consistency
+import dotenv from 'dotenv'
+// import mongoose from "mongoose";
+// import { DB_NAME } from "./constants";
+import connectDB from "./db/index.js"; //APPROACH 2
+
+dotenv.config({
+    path:'./env'
+})
+
+
+connectDB()
+
+
+
+
+
+
+/*APPPROACH 1
+import express from "express";
+const app = express()
+;( async ()=>{
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        app.on("error",(error)=>{
+            console.log("ERRR:",error);
+        })
+        app.listen(process.env.PORT,()=>{
+            console.log(`Listening on port ${process.env.PORT}`);
+        })
+    } catch (error) {
+        console.error("Error: ",error)
+        throw err;
+    }
+})() //ifee - immediately invoked function expression
+ */
