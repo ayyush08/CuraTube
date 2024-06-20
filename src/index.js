db9d7c7c7ebb35244876349d84affcd64eec9c1a
ayyush08 <ayushkumargupta2908@gmail.com>
2024-06-20 11:59:26 +0530

    setup configured - user router and controller

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv'
 // import mongoose from "mongoose";
 // import { DB_NAME } from "./constants";
 import connectDB from "./db/index.js"; //APPROACH 2
-
+import { app } from './app.js';
 dotenv.config({
     path:'./env'
 })
