15f2f6eec3ca305a183a927b6f94558f89b1ae86
ayyush08 <ayushkumargupta2908@gmail.com>
2024-06-27 16:31:20 +0530

    feat: Add change password functionality and update user profile endpoints

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
-import { loginUser, logoutUser, registerUser,refreshAccessToken } from "../controllers/user.controller.js";
+import { loginUser, logoutUser, registerUser,refreshAccessToken, changeCurrentPassword, getCurrentUser, updateAccountDetails, updateUserAvatar, updateUserCoverImage, getUserChannelProfile, getWatchHistory } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 
 
@@ -26,6 +26,19 @@ router.route('/login').post(loginUser)
 //secured routes
 router.route('/logout').post(verifyJWT, logoutUser)
 router.route('/refresh-token').post(refreshAccessToken) //verifyjwt ni lgaya kyuki controller me hi decode kr rhe h refrshaccestoken function me
+router.route('/change-password').post(verifyJWT, changeCurrentPassword)
+
+router.route('/current-user').get(verifyJWT,getCurrentUser);
+
+router.route('/update-account').patch(verifyJWT,updateAccountDetails) //post rkhne se sari update hojatis
+router.route('/avatar').patch(verifyJWT,upload.single('avatar'),updateUserAvatar)
+
+
+router.route('/cover-image').patch(verifyJWT,upload.single('coverImage'),updateUserCoverImage)
+
+router.route('/channel/:username').get(verifyJWT,getUserChannelProfile)
+
+router.route('/watch-history').get(verifyJWT,getWatchHistory)
 
 
 export default router
