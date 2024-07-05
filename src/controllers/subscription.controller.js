4b48617bf92b83e5994a0018facd10953ee67c46
ayyush08 <ayushkumargupta2908@gmail.com>
2024-07-05 23:33:31 +0530

    Add endpoint to fetch subscribers list for a channel

--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -39,6 +39,58 @@ const toggleSubscription = asyncHandler(async (req, res) => {
 // controller to return subscriber list of a channel
 const getUserChannelSubscribers = asyncHandler(async (req, res) => {
     const {channelId} = req.params
+    if(!isValidObjectId(channelId)){
+        throw new ApiError(400, "Invalid channel id")
+    }
+    const subsList = await Subscription.aggregate([
+        {
+            $match:{
+                channel:mongoose.Types.ObjectId(channelId)
+            
+            },
+        },
+        {
+            $lookup:{
+                from:'users',
+                localField:'subscriber',
+                foreignField:'_id',
+                as:'subscriber',
+                pipeline:[
+                    {
+                        $project:{
+                            _id:1,
+                            name:1,
+                            email:1,
+                            profilePic:1
+                        }
+                    }
+                ]
+            },
+        },
+            {
+                $unwind:'$subscriber',
+            },
+            {
+                $project:{
+                    _id:0,
+                    subscriber:{
+                        _id:1,
+                        username:1,
+                        email:1,
+                        avatar:1
+                    
+                    },
+                }
+            
+            }
+        
+    ])
+    if(!subsList){
+        throw new ApiError(404, "No subscribers found")
+    }
+    return res
+    .status(200)
+    .json( new ApiResponse(200,subsList, "Subscribers list fetched successfully"))
 })
 
 // controller to return channel list to which user has subscribed
