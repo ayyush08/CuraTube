37b436ae5dcdd1970c33cc16f4575de2a4663ea4
ayyush08 <ayushkumargupta2908@gmail.com>
2024-07-19 07:46:30 +0530

    feat: Update  tweet content and Delete tweet functionality in tweet.controller.js

--- src/controllers/tweet.controller.js
+++ src/controllers/tweet.controller.js
@@ -52,10 +52,42 @@ const getUserTweets = asyncHandler(async (req, res) => {
 
 const updateTweet = asyncHandler(async (req, res) => {
     //TODO: update tweet
+    const {tweetId} = req.params
+    const {content}= req.body
+
+    if(!content)
+        throw new ApiError(400,"Content is required")
+    if(!isValidObjectId(tweetId))
+        throw new ApiError(400,"Invalid tweet id")
+    const tweet = await Tweet.findById(tweetId)
+    if(!tweet)
+        throw new ApiError(404,"Tweet not found")
+
+    if(tweet?.owner.toString()!==req.user?._id.toString())
+        throw new ApiError(403,"You are not authorized to update this tweet")
+    const updatedTweet = await Tweet.findByIdAndUpdate(tweetId,{
+        $set:{ content }
+    },
+        { new:true }
+    )
+
+    res
+    .status(200)
+    .json(new ApiResponse(200,updatedTweet,"Tweet updated successfully"))
 })
 
 const deleteTweet = asyncHandler(async (req, res) => {
     //TODO: delete tweet
+    const {tweetId} = req.params
+    if(!isValidObjectId(tweetId))
+        throw new ApiError(400,"Invalid tweet id")
+    const tweet = await Tweet.findById(tweetId)
+    if(!tweet)
+        throw new ApiError(404,"Tweet not found")
+    await Tweet.findByIdAndDelete(tweetId)
+    res
+    .status(200)
+    .json(new ApiResponse(200,null,"Tweet deleted successfully"))
 })
 
 export {
