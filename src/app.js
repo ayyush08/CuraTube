d46ff4b815824304902150988122545990fb10d6
AYUSH KUMAR GUPTA <ayushkumargupta2908@gmail.com>
2025-04-27 03:10:14 +0530

    feat: healthcheck controller

--- src/app.js
+++ src/app.js
@@ -16,6 +16,10 @@ app.use(express.urlencoded({extended: true,limit: "16kb"}));
 app.use(express.static("public"));
 app.use(cookieParser())
 
+app.get("/", (req, res) => {
+    res.status(200).json({message: "Welcome to the CuraTube API"})
+})
+
 //routes import
 import userRouter from "./routes/user.routes.js";
 import healthcheckRouter from "./routes/healthcheck.routes.js"
@@ -27,7 +31,9 @@ import likeRouter from "./routes/like.routes.js"
 import playlistRouter from "./routes/playlist.routes.js"
 import dashboardRouter from "./routes/dashboard.routes.js"
 //routes declaration
-app.use('/api/v1/users',userRouter)
+
+app.use("/api/v1/healthcheck", healthcheckRouter)
+
 app.use("/api/v1/users", userRouter)
 app.use("/api/v1/tweets", tweetRouter)
 app.use("/api/v1/subscriptions", subscriptionRouter)
