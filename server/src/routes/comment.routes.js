230e46a512c54974ed232a3afa405a14761b5193
AYUSH KUMAR GUPTA <ayushkumargupta2908@gmail.com>
2025-08-05 19:05:48 +0530

    dockrize

--- server/src/routes/comment.routes.js
+++ server/src/routes/comment.routes.js
@@ -9,7 +9,7 @@ import {verifyJWT} from "../middlewares/auth.middleware.js"
 
 const router = Router();
 
-router.use(verifyJWT); 
+router.use(verifyJWT);
 
 router.route("/:videoId")
 .get(getVideoComments)//done
